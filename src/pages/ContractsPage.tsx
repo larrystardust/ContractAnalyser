e209c847c7a6405fee0c00024761f5fd4d7b2655
larrystardust <larry@5starsrating.com>
2025-09-14 16:30:09 +0100

    Updated ContractsPage.tsx

--- src/pages/ContractsPage.tsx
+++ src/pages/ContractsPage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ContractList from '../components/contracts/ContractList';
+import { useTranslation } from 'react-i18next'; // ADDED
 
 const ContractsPage: React.FC = () => {
+  const { t } = useTranslation(); // ADDED
   // This function is passed to ContractList, but for this page,
   // we don't need to perform any specific action on contract selection.
   // In a more complex application, clicking a contract here might navigate
@@ -12,7 +14,7 @@ const ContractsPage: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-6 mt-16">
-      <h1 className="text-2xl font-bold text-gray-900 mb-6">Your Contracts</h1>
+      <h1 className="text-2xl font-bold text-gray-900 mb-6">{t('your_contracts')}</h1> {/* MODIFIED */}
       <ContractList /> {/* MODIFIED: Removed onSelectContract prop */}
     </div>
   );
