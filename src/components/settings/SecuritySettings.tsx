d092cbcc7b1665e668d6f471d2939a153e73e6d6
larrystardust <larry@5starsrating.com>
2025-09-14 16:42:29 +0100

    Updated SecuritySettings.tsx

--- src/components/settings/SecuritySettings.tsx
+++ src/components/settings/SecuritySettings.tsx
@@ -361,7 +361,7 @@ const SecuritySettings: React.FC = () => {
                   type={showPasswords.current ? 'text' : 'password'}
                   required
                   className="w-full pl-10 pr-10 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  placeholder={t('current_password')} {/* MODIFIED */}
+                  placeholder={t('current_password')} 
                   value={passwordData.currentPassword}
                   onChange={handlePasswordChange}
                 />
