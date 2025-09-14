b9a667f40e5cd350d202e49c8318d06de67465e6
larrystardust <larry@5starsrating.com>
2025-09-14 16:43:51 +0100

    Updated SecuritySettings.tsx

--- src/components/settings/SecuritySettings.tsx
+++ src/components/settings/SecuritySettings.tsx
@@ -646,7 +646,7 @@ const SecuritySettings: React.FC = () => {
                 type="password"
                 required
                 className="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                placeholder={t('your_password_modal')} {/* MODIFIED */}
+                placeholder={t('your_password_modal')} 
                 value={reauthPassword}
                 onChange={(e) => setReauthPassword(e.target.value)}
                 disabled={isLoading}
