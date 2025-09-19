fd9535baea769226f3f85f585d80f15c5c86eebf
larrystardust <larry@5starsrating.com>
2025-09-19 19:18:25 +0100

    Updated ProfileSettings.tsx

--- src/components/settings/ProfileSettings.tsx
+++ src/components/settings/ProfileSettings.tsx
@@ -306,7 +306,7 @@ const ProfileSettings: React.FC = () => {
                     value={formData.mobilePhoneNumber}
                     onChange={handleInputChange}
                     className="w-full px-3 py-2 border border-gray-300 rounded-r-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                    placeholder="e.g., 1234567890"
+                    placeholder={t('phone_number_example')} // MODIFIED: Translated placeholder
                   />
                 </div>
               </div>
