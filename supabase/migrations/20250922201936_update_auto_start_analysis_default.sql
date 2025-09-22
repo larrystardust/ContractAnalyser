9dde3b10abb0d5861928c657f71f45c62072c0cd
larrystardust <larry@5starsrating.com>
2025-09-22 21:39:21 +0100

    Updated 20250922201936_update_auto_start_analysis_default.sql

--- supabase/migrations/20250922201936_update_auto_start_analysis_default.sql
+++ supabase/migrations/20250922201936_update_auto_start_analysis_default.sql
@@ -1,10 +1,3 @@
 -- Set the default value of auto_start_analysis_enabled to TRUE
 ALTER TABLE public.profiles
 ALTER COLUMN auto_start_analysis_enabled SET DEFAULT TRUE;
-
--- Optional: Update existing rows that are currently FALSE to TRUE
--- Only run this if you want existing users to have auto-start analysis enabled by default
--- If you want existing users to keep their current setting (FALSE), do NOT run this UPDATE.
-UPDATE public.profiles
-SET auto_start_analysis_enabled = TRUE
-WHERE auto_start_analysis_enabled = FALSE;
