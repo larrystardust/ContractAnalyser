b0b2c139ebed9c6f350853e89c21aa00fecbac7a
larrystardust <larry@5starsrating.com>
2025-09-27 18:40:00 +0100

    Updated blogData.ts

--- src/data/blogData.ts
+++ src/data/blogData.ts
@@ -4,7 +4,6 @@ export interface BlogPost {
   author: string;
   date: string; // Format: YYYY-MM-DD
   excerpt: string;
-  content: string[]; // Array of strings, each representing a paragraph or section
   imageUrl?: string; // Optional image URL for the post header
 }
 
