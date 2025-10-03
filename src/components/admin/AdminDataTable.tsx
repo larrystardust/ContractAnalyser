897013393586f97ec498f80cedc807358cba961f
larrystardust <larry@5starsrating.com>
2025-10-03 09:54:56 +0100

    Updated AdminDataTable.tsx

--- src/components/admin/AdminDataTable.tsx
+++ src/components/admin/AdminDataTable.tsx
@@ -19,7 +19,7 @@ interface AdminDataTableProps<T> {
   customActions?: (item: T) => React.ReactNode;
 }
 
-const AdminDataTable = <T extends {id: string | number },>(
+const AdminDataTable = <T extends {id: string | number},>(
   { data, columns, loading, error, onEdit, onDelete, customActions }: AdminDataTableProps<T>
 ) => {
   const { t } = useTranslation(); // ADDED
