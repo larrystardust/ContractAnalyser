64e1b6a259dd7852043376aeb73483f0ef9f6c91
larrystardust <larry@5starsrating.com>
2025-10-06 09:09:48 +0100

    Updated PrivacyPolicyPage.tsx

--- src/pages/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next'; // ADDED
 import { Helmet } from 'react-helmet-async'; // ADDED: Import Helmet
 
 const PrivacyPolicyPage: React.FC = () => {
-  const { t } = useTranslation(); // ADDED
+  const { t, i18n } = useTranslation(); // MODIFIED: Destructure i18n
 
   const webPageSchema = {
     "@context": "https://schema.org",
@@ -24,6 +24,8 @@ const PrivacyPolicyPage: React.FC = () => {
   return (
     <>
       <Helmet> {/* ADDED: Helmet for meta description */}
+        <html lang={i18n.language} /> {/* ADDED: lang attribute */}
+        <title>{t('privacy_policy_title')}</title> {/* ADDED: Dynamic title */}
         <meta name="description" content={t('privacy_policy_page_meta_description')} />
       </Helmet>
       <StructuredData schema={webPageSchema} />
