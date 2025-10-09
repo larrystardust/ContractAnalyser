b8e3e1521a56c507f8e2981f107200e3a966069b
larrystardust <larry@5starsrating.com>
2025-10-09 13:21:59 +0100

    Updated PricingSection.tsx

--- src/components/pricing/PricingSection.tsx
+++ src/components/pricing/PricingSection.tsx
@@ -93,7 +93,7 @@ const PricingSection: React.FC = () => {
       {productSchema.map((schema, index) => (
         <StructuredData key={index} schema={schema} />
       ))}
-      <div className="py-12 bg-gray-50 mt-16 dark:bg-gray-700 dark:text-gray-200"> {/* MODIFIED: Added dark mode styles */} 
+      <div className="py-12 bg-gray-50 mt-16 dark:bg-gray-700 dark:text-gray-400"> {/* MODIFIED: Added dark mode styles */} 
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center">
             <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white sm:text-4xl"> {/* MODIFIED */}
