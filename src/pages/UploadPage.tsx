db005007c60c80f754433555467828ab2ce3ef42
larrystardust <larry@5starsrating.com>
2025-10-22 11:19:54 +0100

    Updated UploadPage.tsx

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -188,7 +188,7 @@ const UploadPage: React.FC = () => {
           canPerformAdvancedAddon={canPerformAdvancedAddon}
           ocrCost={OCR_COST}
           basicAnalysisCost={BASIC_ANALYSIS_COST}
-          advancedAnalysisAddonCost={ADVANCED_ANALYSIS_ADDON_COST}
+          advancedAnalysisAddonCost={ADVANCED_ANALYSIS_ADDON_COST} {/* MODIFIED: Pass as prop */}
           showProcessingOptions={showProcessingOptions}
           isAdvancedSubscription={isAdvancedSubscription}
           isBasicSubscription={isBasicSubscription}
