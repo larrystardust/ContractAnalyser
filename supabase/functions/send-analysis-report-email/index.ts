beb3d4d4f4ab2f26444cdc1427a52795cc04f35b
larrystardust <larry@5starsrating.com>
2025-11-13 16:28:10 +0000

    Updated index.ts

--- supabase/functions/send-analysis-report-email/index.ts
+++ supabase/functions/send-analysis-report-email/index.ts
@@ -86,7 +86,10 @@ Deno.serve(async (req) => {
           <p>${message}</p>
           ${reportLink && reportLink !== 'N/A' ? `
             <p>${getTranslatedMessage('email_view_full_report', userPreferredLanguage)}</p>
-            <p><a href="${publicReportViewerUrl}" target="_blank">${getTranslatedMessage('email_view_full_report_button', userPreferredLanguage)}</a></p>
+            <p><a href="${publicReportViewerUrl}" target="_blank">${getTranslatedMessage('email_view_full_report_button', userPreferredLanguage)}</a></p>           
+            ${redlinedClauseArtifactPath && performedAdvancedAnalysis ? `
+              <p>${getTranslatedMessage('email_view_redlined_artifact', userPreferredLanguage)}</p>
+              <p><a href="${redlinedArtifactLink}" target="_blank">${getTranslatedMessage('email_view_redlined_artifact_button', userPreferredLanguage)}</a></p>
             ` : ''}
             <hr/>
             ${reportHtmlContent}
