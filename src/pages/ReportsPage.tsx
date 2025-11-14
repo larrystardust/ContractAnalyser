4026ea221beac3691c9f0d0ffc5443b2e5760abe
larrystardust <larry@5starsrating.com>
2025-11-14 13:23:49 +0000

    Updated ReportsPage.tsx

--- src/pages/ReportsPage.tsx
+++ src/pages/ReportsPage.tsx
@@ -183,7 +183,7 @@ const ReportsPage: React.FC = () => {
                       return translatedValue;
                     })()}
                   </p>
-                  <p className="text-3xl font-bold text-gray-900">{number}</p>
+                  <p className="text-3xl font-bold text-gray-900">{0}</p>
                 </CardBody>
               </Card>
             ))}
